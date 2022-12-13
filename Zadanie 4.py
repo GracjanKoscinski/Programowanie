e8daa0be27d1138f766ac71b8837a3adc74f41cd
GracjanKoscinski <116656782+GracjanKoscinski@users.noreply.github.com>
2022-12-13 20:58:41 +0100

    Update Zadanie 4.py

--- Zadanie 4.py
+++ Zadanie 4.py
@@ -1,7 +1,7 @@
 lista = []
 
 
-def CREATE(imie,nazwisko,pesel):
+def CREATE(imie, nazwisko, pesel):
     return{
         'imie': imie,
         'nazwisko': nazwisko,
@@ -9,7 +9,7 @@ def CREATE(imie,nazwisko,pesel):
     }
 
 
-def UPDATE(imie,nazwisko,pesel,lista):
+def UPDATE(imie, nazwisko, pesel, lista):
     for i in range(len(lista)):
         if pesel == lista[i]['pesel']:
             lista[i]['imie'] = imie
@@ -17,9 +17,27 @@ def UPDATE(imie,nazwisko,pesel,lista):
     return lista
 
 
+def READ(imie, lista):
+    for i in range(len(lista)):
+        if imie == lista[i]['imie']:
+            print(lista[i])
+            return 0
+    print("Nie ma takiej osoby na liście")
+    return 0
+
+def DELETE(pesel, lista):
+    for i in range(len(lista)):
+        if pesel == lista[i]['pesel']:
+            lista[i].pop('imie')
+            lista[i].pop('nazwisko')
+            lista[i].pop('pesel')
+    return lista
+
+
 lista.append(CREATE('Gracjan', 'Koscinski', '132313213'))
 lista.append(CREATE('Gracjan', 'Koscinski', '1322313213'))
 
 UPDATE('Edward', 'Kos', '132313213', lista)
-
+READ('Gracjan', lista)
+DELETE('132313213', lista)
 print(lista)
